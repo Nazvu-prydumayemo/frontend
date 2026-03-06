32296d713593b3cc7f8f6a673c644410627f33e8
ItakawaM <maksymworkp@gmail.com>
2026-03-06 17:14:29 +0200

    NP-42 Create TUIApp class

--- src/tuiapp/app.py
+++ src/tuiapp/app.py
@@ -1,18 +1,28 @@
+from pathlib import Path
+from typing import ClassVar
+
 from textual.app import App
-from textual.containers import Vertical
-from textual.screen import Screen
-from textual.widgets import Button
+from textual.binding import Binding
 
 
-class TUIApplication(App[Screen]):
-    """Minimal Textual App skeleton for mypy"""
+class TUIApplication(App):
+    """
+    - Root application class
+    """
 
-    def compose(self):
-        """Define the UI layout."""
-        yield Vertical(
-            Button("Login"),
-            Button("Register"),
-        )
+    CSS_PATH = Path("styles") / Path("styles.tcss")
+    TITLE = "Tennis App"
+    SUB_TITLE = "Tennis App Local Client"
+
+    BINDINGS: ClassVar[list] = [
+        Binding("escape", "pop_screen", "Back"),
+    ]
+
+    def on_mount(self) -> None:
+        """
+        - Mounts first page
+        """
+        pass
 
 
 if __name__ == "__main__":
