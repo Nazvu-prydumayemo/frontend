f78582bfffca6fcd74b51d3b7deaba2ae3da94d3
Khrystyna Romaniv <terrkris1111@gmail.com>
2026-03-11 00:33:14 +0200

    NP-28 Create Register Page

--- src/tuiapp/widgets/inputs.py
+++ src/tuiapp/widgets/inputs.py
@@ -1,10 +1,10 @@
 from textual.widgets import Input
 
 class CustomInput(Input):
-    
+    """Кастомний інпут для тексту"""
     pass
 
 class PasswordInput(Input):
-   
+    """Кастомний інпут для паролів"""
     def __init__(self, **kwargs):
         super().__init__(password=True, **kwargs)
