1ec4eb6179ca7097ad6c379a288c415525449da8
ItakawaM <maksymworkp@gmail.com>
2026-03-13 20:41:03 +0200

    NP-90 Create Base View class

---
+++ src/tuiapp/widgets/views/base_view.py
@@ -0,0 +1,29 @@
+from abc import abstractmethod
+
+from textual.app import ComposeResult
+from textual.containers import Container
+
+
+class BaseView(Container):
+    """Base class for all views."""
+
+    def compose(self) -> ComposeResult:
+        with Container(classes="view-container"):
+            yield from self.compose_view()
+
+    """Each subclass should handle button events of the view."""
+
+    @abstractmethod
+    def compose_view(self) -> ComposeResult:
+        """Each subclass should define the actual content of the view."""
+        pass
+
+    @abstractmethod
+    def on_view_activated(self) -> None:
+        """Each subclass should define what happens when the view is activated."""
+        pass
+
+    @abstractmethod
+    def on_view_closed(self) -> None:
+        """Each subclass should define what happens the view is closed."""
+        pass
