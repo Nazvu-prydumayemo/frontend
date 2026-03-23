e2946d09881e5f45417664018d77e24780d1a9ea
Khrystyna Romaniv <terrkris1111@gmail.com>
2026-03-24 00:42:49 +0200

    restore login_form to previous working version

--- src/tuiapp/widgets/forms/login_form.py
+++ src/tuiapp/widgets/forms/login_form.py
@@ -1,8 +1,10 @@
+from pydantic import ValidationError
 from textual.app import ComposeResult
 from textual.containers import Vertical
 from textual.widget import Widget
 from textual.widgets import Static
 
+from tuiapp.api.auth.schema import LoginRequest
 from tuiapp.widgets.inputs import PasswordInput, TextInput
 
 
@@ -19,13 +21,19 @@ class LoginForm(Widget):
                 yield Static("Password", classes="field-label")
                 yield PasswordInput(placeholder="Password", id="password")
 
-    def get_data(self) -> dict[str, str]:
+    def get_data(self) -> LoginRequest | str:
         """Get the form data.
 
         Returns:
-            A dictionary with email and password values.
+            A pydantic model with email and password values or string error if invalid data was provided.
         """
-        return {
-            "email": self.query_one("#email", TextInput).value,
-            "password": self.query_one("#password", PasswordInput).value,
-        }
+        email = self.query_one("#email", TextInput).value
+        password = self.query_one("#password", PasswordInput).value
+        if not email or not password:
+            return "All fields required"
+
+        try:
+            return LoginRequest(email=email, password=password)
+
+        except ValidationError:
+            return "Invalid email format"
